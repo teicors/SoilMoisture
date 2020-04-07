ae3b122fc41b68b41a7faaa10cf50f4b87e55b47
teicors <corsaroangelo@gmail.com>
2020-04-07 10:54:11 +0200

    Add files via upload

---
+++ component.mk
@@ -0,0 +1,13 @@
+ARDUINO_LIBRARIES := LiquidCrystal DHTesp ArduinoJson5 Timezone SolarCalculator
+COMPONENT_SEARCH_DIRS := /opt/sming/Components
+COMPONENT_DEPENDS := Default ShiftRegister74HC595
+COMPONENT_DEPENDSop := Adafruit_ST7735_AS CronLibrary Default
+COMPONENT_DEPENDSss := CronLibrary
+COMPONENT_DEPENDSaa := Default
+_COMPONENT_DEPENDS := MySQL_Connector_Arduino
+
+DEBUG_VERBOSE_LEVEL = 3
+SPI_SIZE = 4M
+
+SPIFF_FILES = web
+
